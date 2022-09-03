34751a2e52947e093b34fd7355e3273347312301
Musfiq <musfiqrumel@gmail.com>
2022-09-03 07:39:04 +0600

    modal full ready

--- js/main.js
+++ js/main.js
@@ -2,10 +2,16 @@
 
 const newApi = async() =>{
     const url = `https://openapi.programming-hero.com/api/news/categories`;
-    const res = await fetch(url);
-    const data = await res.json();
-    // console.log(data.data.news_category);
-    displayCategoryMenu(data.data.news_category);
+    try{
+      const res = await fetch(url);
+      const data = await res.json();
+      // console.log(data.data.news_category);
+      displayCategoryMenu(data.data.news_category);
+    }
+    catch(error){
+      console.log(error)
+    }
+    
 }
 
 const displayCategoryMenu = categories =>{
@@ -36,7 +42,7 @@ const categoryPost = (posts) =>{
     const displayCard = document.getElementById('display-card');    
     displayCard.textContent = '';
     for(let post of posts){
-        console.log(post)
+        // console.log(post)
         const div = document.createElement('div');
         div.classList.add('row', 'g-4');
         div.innerHTML=`
@@ -49,7 +55,7 @@ const categoryPost = (posts) =>{
               <p class="card-text">${post.details.substring(0, 350) + '.....'}</p>
               <img style="height: 40px;" src="${post.author.img}" class="rounded-circle float-start" alt="..."><p class="card-text"><small class="text-muted">${post.author.name}</small></p>
               <div class="text-end">                  
-              <button onclick="showModal(${post._id})" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Launch demo modal</button>
+              <button onclick="showModal('${post._id}')" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">More details</button>
               </div> 
               <p class="text-center">View: ${post ? post.total_view : 'no data found' }</p>
                 
@@ -68,27 +74,42 @@ const modalApi = async(news_id) =>{
     const res = await fetch(url);
     const data = await res.json();
     showModal(data.data)
+    // console.log(data.data)
 }
 
 const showModal = (modals) => {
-     const modalArea = document.getElementById('exampleModal');
-      
-     modals.forEach(modal => {
-      const div = document.createElement('div');
-      div.classList.add('modal-dialog', 'modal-dialog-centered');
-      div.innerHTML=`
-      
-      `
+     const modalArea = document.getElementById('modal-div');     
+     console.log(modals);
 
-      console.log(modal, modals);
-     });
+     const div = document.createElement('div');
+     div.classList.add('modal-content');
+     div.innerHTML=`
+     <div class="modal-header">
+     <h5 class="modal-title" id="exampleModalLabel">${modals[0].title}</h5>
+     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+   </div>
+   <div class="modal-body">
+     ${modals[0].details}
+   </div>
+   <img style="height: 40px; width: 50px;" src="${modals[0].author.img}" class="rounded-circle float-start" alt="..."><p class="card-text"><small class="text-muted">${modals[0].author.name}</small></p>
+   <p class="text-center">View: ${modals[0].author ? modals[0].author.published_date : 'no data found' }</p>
+   <div class="modal-footer">   
+     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+   </div>
+     `
+   modalArea.appendChild(div);
+
+
+    // for(let modal of modals){
+    //   console.log(modal)
+    // }
+     
+    //  modals.forEach(modal => {
+    //   console.log(modal, modals);      
+    //  });
 
 }
+// showModal();
 
-
-// modalApi('0282e0e58a5c404fbd15261f11c2ab6a');
-
-
-
-
+modalApi('0282e0e58a5c404fbd15261f11c2ab6a');
 newApi();
